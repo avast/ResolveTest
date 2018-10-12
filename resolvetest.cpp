ca879e4e93dd132bfabf029756adde780ef76360
Karas Lukáš <karas@avast.com>
2018-10-12 17:22:52 +0200

    add DNS query

--- resolvetest.cpp
+++ resolvetest.cpp
@@ -1,16 +1,19 @@
-// g++ --std=c++11 -o resolvetest resolvetest.cpp
+// g++ --std=c++11 -o resolvetest resolvetest.cpp -lresolv
 
 #include <string>
 #include <iostream>
 #include <stdexcept>
 #include <cassert>
 #include <mutex>
+#include <vector>
+#include <cstring>
 
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <arpa/nameser.h>
+#include <resolv.h>
 
 std::string inet4str(const in_addr &in4) {
   char addr_str[INET_ADDRSTRLEN];
@@ -55,6 +58,150 @@ bool resolve(int af, const std::string &resolveName){
   return true;
 }
 
+#define RESOLV_STATE (_res)
+
+bool parse_record(ns_msg &msg, const int rrnum){
+  ns_rr rr;
+  if (ns_parserr(&msg, ns_s_an, rrnum, &rr) < 0)
+  {
+    std::cerr << "ns_parserr" << std::endl;
+    return false;
+  }
+  int rdlen = ns_rr_rdlen(rr);
+
+  std::cout << ns_rr_name(rr) << " ";
+
+  // Decode data field (not done by ns_parserr)
+  switch (ns_rr_type(rr))
+  {
+    case ns_t_a:
+      // IPv4 address
+      std::cout << "A: " << inet4str(*((in_addr *)ns_rr_rdata(rr)));
+      break;
+    case ns_t_aaaa:
+      // IPv6 address
+      std::cout << "AAA: " << inet4str(*((in_addr *)ns_rr_rdata(rr)));
+      break;
+    case ns_t_txt:
+    {
+      // <character-string>: length (1 octet), string
+      if (rdlen < 1) {
+        std::cerr << "Invalid TXT record (no data)";
+        return false;
+      }
+      size_t length = *ns_rr_rdata(rr);
+      if (rdlen < 1 + (int)length) {
+        std::cerr << "Invalid TXT record";
+        return false;
+      }
+      std::cout << "TXT: " << std::string((const char *)(ns_rr_rdata(rr) + 1), length);
+      break;
+    }
+    case ns_t_ns:
+    case ns_t_cname:
+    {
+      // <domain-name> (compressed)
+      char nsname[NS_MAXDNAME];
+      if (ns_name_uncompress(ns_msg_base(msg), ns_msg_end(msg),
+                             ns_rr_rdata(rr), nsname, NS_MAXDNAME) < 0)
+      {
+        std::cerr << "ns_name_uncompress failed" << std::endl;
+        return false;
+      }
+      std::cout << "CNAME: " << nsname;
+      break;
+    }
+    case ns_t_mx:
+    {
+      // MX: preference (2 octets), <domain-name> (compressed)
+      char nsname[NS_MAXDNAME];
+      uint16_t preference = ntohs(*(uint16_t *) ns_rr_rdata(rr));
+      if (ns_name_uncompress(ns_msg_base(msg), ns_msg_end(msg),
+                             ns_rr_rdata(rr) + 2, nsname, NS_MAXDNAME) < 0)
+      {
+        std::cerr << "ns_name_uncompress failed" << std::endl;
+        return false;
+      }
+      std::cout << "MX: " << nsname;
+      std::cout << "(mx_preference = " << preference << ")";
+      break;
+    }
+    case ns_t_soa:
+      assert(!"Dns: SOA parsing - not implemented");
+      break;
+    case ns_t_srv:
+      assert(!"Dns: SRV parsing - not implemented");
+      break;
+    default:
+      // Other fields: no data copied
+      std::cout << "TYPE " << ns_rr_type(rr);
+      break;
+  }
+
+  std::cout << std::endl << "  ";
+  std::cout << "TTL " << ns_rr_ttl(rr) << " ";
+  std::cout << "CLASS " << ns_rr_class(rr) << " ";
+  std::cout << std::endl;
+
+  return true;
+}
+
+
+bool query(int type, const std::string &resolveName, int cls = ns_c_in){
+
+  std::vector<u_char> ansbuf(1024, 0);
+
+  for (;;) {
+    // Repeat the query until our buffer is large enough for the response
+    int anslen = res_query(resolveName.c_str(), cls, type, ansbuf.data(), (int) ansbuf.size());
+    if (anslen == -1) {
+      if (RESOLV_STATE.res_h_errno == NO_DATA) {
+        // Valid name, no data record of requested type
+        // This is success, but we don't yield any record
+        std::cout << "no data" << std::endl;
+        return true;
+      }
+      std::cerr << "res_query(" << resolveName << ", " << type << "): " << hstrerror(RESOLV_STATE.res_h_errno) << std::endl;
+      return false;
+    }
+    // Was our buffer large enough?
+    if (anslen > (int)ansbuf.size()) {
+      // Grow to requested size
+      ansbuf.resize((size_t) anslen);
+    } else {
+      // Shrink to actual size of the response
+      ansbuf.resize((size_t) anslen);
+      break;
+    }
+  }
+
+  ns_msg msg;
+  int rc = ns_initparse(ansbuf.data(), (int) ansbuf.size(), &msg);
+  if (rc == -1) {
+    std::cerr << "ns_initparse(): " << hstrerror(RESOLV_STATE.res_h_errno) << std::endl;
+    return false;  // Could not parse
+  }
+
+  rc = ns_msg_getflag(msg, ns_f_rcode);
+  if (rc != ns_r_noerror) {
+    // We will never get here, because errors are handled by res_nquery.
+    // If we did (by sending query in another way), we'd get finer grained
+    // error codes as defined in nameser.h - ns_r_*
+    std::cerr << "query error: " << rc << std::endl;
+    return false;
+  }
+
+  for (int rrnum = 0; rrnum < ns_msg_count(msg, ns_s_an); rrnum++)
+  {
+    if (!parse_record(msg, rrnum)) {
+       std::cerr << "could not parse response";
+       return false;
+    }
+  }
+
+  return true;
+}
+
 int main(int argc, const char* args[]){
   // Libresolv does not work with uClibc 0.9.32 due to bug, see:
   // http://lists.busybox.net/pipermail/uclibc-cvs/2012-July/030778.html
@@ -68,11 +215,27 @@ int main(int argc, const char* args[]){
   }
   std::cout << "Resolving " << resolveName << std::endl;
 
+  std::cout << std::endl << "System resolve:" << std::endl;
+
   if (resolve(AF_INET, resolveName) && resolve(AF_INET6, resolveName)) {
     std::cout << "OK" << std::endl;
-    return 0;
   }else{
-    std::cout << "FAILURE" << std::endl;
+    std::cerr << "FAILURE" << std::endl;
     return 1;
   }
+
+  if (res_init() != 0){
+    std::cerr << "res_init FAILURE" << std::endl;
+    return 1;
+  }
+
+  std::cout << std::endl << "DNS query:" << std::endl;
+  if (query(AF_INET, resolveName) && query(AF_INET6, resolveName)){
+    std::cout << "OK" << std::endl;
+  }else{
+    std::cerr << "query FAILURE" << std::endl;
+    return 1;
+  }
+
+  return 0;
 }
